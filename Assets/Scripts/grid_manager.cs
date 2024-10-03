a246eb82fe7e1224bfcc586fa77776921da21d5b
spevenexe <terrence.li.exe@gmail.com>
2024-10-03 02:08:46 -0500

    Added multi-dimensional shapes

--- Assets/Scripts/grid_manager.cs
+++ Assets/Scripts/grid_manager.cs
@@ -12,9 +12,9 @@ public class grid_manager : MonoBehaviour
     public Tilemap map; // place the floor tilemap here. This contains the legal placement area
     public BoundsInt area; // The x,y bounds of the surrounding rectanle around the legal placement area
     // public Dictionary<Vector3Int,ClickableTile> slots;
-    [DoNotSerialize] public List<ClickableTile> slots;
-    public ClickableTile[] prefilled_tiles;
-    public ClickableTile ctile_template;
+    [DoNotSerialize] public List<Prop> props;
+    public Prop[] prefilled_props;
+    public Prop selected_prop;
 
 
     private GridLayout grid;
@@ -24,36 +24,60 @@ public class grid_manager : MonoBehaviour
         
         // the shape of the level is not guaranteed to be perfectly rectangular, so a dictionary is better
         // slots = new Dictionary<Vector3Int, ClickableTile>();
-        slots = new List<ClickableTile>();
+        props = new List<Prop>();
 
-        foreach(ClickableTile t in prefilled_tiles){
-            slots.Add(t);
-            Debug.Log("slot added to " + t.gridLocation);
-        }
-
-        for(int i = area.min.x; i < area.max.x; i++){
-            for(int j = area.min.y; j < area.max.y; j++){
-                Vector3Int gridPos = new Vector3Int(i,j);
-                // this makes sure to ignore tiles that are not rendered. In other words, illegal locations
-                if(map.GetSprite(gridPos) == null || cellCovered(gridPos))
-                    continue;
-                Vector3 cell_center = map.GetCellCenterWorld(gridPos);
-                cell_center.z-=2;
-
-                ClickableTile ctile = Instantiate(ctile_template,cell_center, Quaternion.identity);
-                ctile.gridLocation = gridPos;
-                slots.Add(ctile);
-            }
+        // for the props already on the screen
+        foreach(Prop p in prefilled_props){
+            props.Add(p);
+            // Debug.Log("slot added to " + p.gridLocation);
         }
     }
 
-    public bool cellCovered(Vector3Int pos){
-        foreach(ClickableTile t in slots){
-            if (t.gridLocation == pos)
-                return true;
+    /// <summary>
+    /// Find the item located at a position in the tile map
+    /// </summary>
+    /// <param name="pos">a Vector3Int representing the location to search the tilemap</param>
+    /// <returns>The object in the tile, or null if there is nothing</returns>
+    public Prop getItemAt(Vector3Int pos){
+        foreach(Prop p in props){
+            foreach(Vector3Int cell in p.shape){
+                if(pos == p.gridLocation + cell){
+                    return p;
+                }
+            }
         }
-        return false;
+        return null;
     } 
+
+    /// <summary>
+    /// remove the item in the list located at pos
+    /// </summary>
+    /// <param name="pos">a Vector3Int representing the location to search the tilemap</param>
+    public void deleteItemAt(Vector3Int pos){
+        Prop target_prop = getItemAt(pos);
+        if(target_prop != null){
+            props.Remove(target_prop);
+            Destroy(target_prop.gameObject);
+            // Debug.Log("destroyed prop");
+        }
+    }
+
+    /// <summary>
+    /// Creates an item located in pos and adds to the list of items present in the level
+    /// </summary>
+    /// <param name="pos"></param>
+    public void createItemAt(Vector3Int pos){
+        // redundant since this condition is checked in Update(), but may be useful to keep it within function?
+        if(map.GetSprite(pos) == null || getItemAt(pos) is not null)
+                    return;
+        Vector3 cell_center = map.GetCellCenterWorld(pos);
+        cell_center.z-=2;
+
+        Prop new_prop = Instantiate(selected_prop,cell_center, Quaternion.identity);
+        new_prop.gridLocation = pos; // this may be unnecessary. See Start() in prop.cs
+        props.Add(new_prop);
+        // Debug.Log("set prop to " + new_prop);
+    }
 
     void Update()
     {
@@ -67,11 +91,10 @@ public class grid_manager : MonoBehaviour
             // convert mouse click's position to Grid position
             Vector3Int cellPosition = grid.WorldToCell(pz);
 
+            Debug.Log(getItemAt(cellPosition));
             // if it's a valid and empty cell, create an object
-            if(!cellCovered(cellPosition)){
-                ClickableTile targetTile = slots[cellPosition];
-
-                targetTile.Prop = ctile_template.Prop;
+            if(map.GetSprite(cellPosition) is not null  && !getItemAt(cellPosition)){
+                createItemAt(cellPosition);
 
                 Debug.DrawRay(grid.CellToWorld(cellPosition),Vector3.one,Color.blue,3f);
             }
@@ -88,11 +111,10 @@ public class grid_manager : MonoBehaviour
             // convert mouse click's position to Grid position
             Vector3Int cellPosition = grid.WorldToCell(pz);
 
+            Debug.Log(getItemAt(cellPosition));
             // if it's a valid and filled cell, destroy the prop
-            if(slots.ContainsKey(cellPosition) && slots[cellPosition].Prop != null){
-                ClickableTile targetTile = slots[cellPosition];
-
-                targetTile.Prop = null;
+            if(map.GetSprite(cellPosition) is not null  && getItemAt(cellPosition)){
+                deleteItemAt(cellPosition);
 
                 Debug.DrawRay(grid.CellToWorld(cellPosition),Vector3.one,Color.blue,3f);
             }
--- Assets/Scripts/prop.cs
+++ Assets/Scripts/prop.cs
@@ -1,7 +1,43 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Prop : MonoBehaviour
 {
+    // placement logic
+    public Tilemap map;
+    public Vector3Int gridLocation;
+    public Vector2Int[] shape = {Vector2Int.zero};
+
+    // Stuff for score logic
     public int score = 0;
-    public int [,] shape = {{1}};
+
+    void Start()
+    {
+        if (map != null)
+        {
+            gridLocation = map.WorldToCell(transform.position);
+            // in case the initial placement is slightly offcenter
+            Vector3 cell_center = map.GetCellCenterWorld(gridLocation); 
+            cell_center.z-=2;
+            transform.position = cell_center;
+        }
+    }
+
+    // this may be more computationally efficient. Instead of updating every frame, only check on mouse click. But eh, whatever.
+    private void OnMouseUp()
+    {
+        // left click - get info from selected tile
+        if (Input.GetMouseButtonUp(0))
+        {
+            // get mouse click's position in 2d plane
+            Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pz.z = 0;
+
+            // convert mouse click's position to Grid position
+            Vector3Int cellPosition = map.WorldToCell(pz);
+
+            // set selectedUnit to clicked location on grid
+            Debug.Log("hi" + cellPosition);
+        }
+    }
 }
