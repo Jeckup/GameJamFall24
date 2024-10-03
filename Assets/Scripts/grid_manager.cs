535532c2a17a75dd3e2b5a87d005e405316975c7
spevenexe <112666138+spevenexe@users.noreply.github.com>
2024-10-02 23:17:31 -0500

    branching work off

--- Assets/Scripts/grid_manager.cs
+++ Assets/Scripts/grid_manager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
@@ -10,39 +11,49 @@ public class grid_manager : MonoBehaviour
 {
     public Tilemap map; // place the floor tilemap here. This contains the legal placement area
     public BoundsInt area; // The x,y bounds of the surrounding rectanle around the legal placement area
-    private GridLayout grid;
-
-    public Dictionary<Vector3Int,ClickableTile> slots;
-    public ClickableTile[] slots1;
+    // public Dictionary<Vector3Int,ClickableTile> slots;
+    [DoNotSerialize] public List<ClickableTile> slots;
     public ClickableTile[] prefilled_tiles;
     public ClickableTile ctile_template;
 
+
+    private GridLayout grid;
     void Start()
     {
         grid = map.layoutGrid;
         
         // the shape of the level is not guaranteed to be perfectly rectangular, so a dictionary is better
-        slots = new Dictionary<Vector3Int, ClickableTile>();
+        // slots = new Dictionary<Vector3Int, ClickableTile>();
+        slots = new List<ClickableTile>();
 
         foreach(ClickableTile t in prefilled_tiles){
-            slots.Add(t.gridLocation, t);
+            slots.Add(t);
             Debug.Log("slot added to " + t.gridLocation);
         }
 
         for(int i = area.min.x; i < area.max.x; i++){
             for(int j = area.min.y; j < area.max.y; j++){
-                Vector3Int pos = new Vector3Int(i,j);
+                Vector3Int gridPos = new Vector3Int(i,j);
                 // this makes sure to ignore tiles that are not rendered. In other words, illegal locations
-                if(map.GetSprite(pos) == null || slots.ContainsKey(pos))
+                if(map.GetSprite(gridPos) == null || cellCovered(gridPos))
                     continue;
-                Vector3 cell_center = map.GetCellCenterWorld(pos);
+                Vector3 cell_center = map.GetCellCenterWorld(gridPos);
                 cell_center.z-=2;
 
                 ClickableTile ctile = Instantiate(ctile_template,cell_center, Quaternion.identity);
-                slots[pos] = ctile;
+                ctile.gridLocation = gridPos;
+                slots.Add(ctile);
             }
         }
     }
+
+    public bool cellCovered(Vector3Int pos){
+        foreach(ClickableTile t in slots){
+            if (t.gridLocation == pos)
+                return true;
+        }
+        return false;
+    } 
 
     void Update()
     {
@@ -57,7 +68,7 @@ public class grid_manager : MonoBehaviour
             Vector3Int cellPosition = grid.WorldToCell(pz);
 
             // if it's a valid and empty cell, create an object
-            if(slots.ContainsKey(cellPosition) && slots[cellPosition].Prop == null){
+            if(!cellCovered(cellPosition)){
                 ClickableTile targetTile = slots[cellPosition];
 
                 targetTile.Prop = ctile_template.Prop;
