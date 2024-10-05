89cc238ae90b0903f9ddc04df3d613a93914bf67
spevenexe <112666138+spevenexe@users.noreply.github.com>
2024-10-05 03:10:13 -0500

    created sample office level. Placement, Rotation, and Selection routines complete

--- Assets/Scripts/prop.cs
+++ Assets/Scripts/prop.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Unity.Mathematics;
 using Unity.VisualScripting;
@@ -11,8 +12,11 @@ public class Prop : MonoBehaviour
     // placement logic
     private Tilemap Map;
     private Vector3Int gridLocation;
-    public Vector2Int[] shape = {Vector2Int.zero};
-    [field: NonSerialized] public SpriteRenderer spriteRenderer;
+    // this holds the original shapee. I tried to make this constant but i have no idea what im doing and its 1 AM
+    [SerializeField] private List<Vector2Int> shape = new List<Vector2Int>(); 
+    // theres probably a way to do this without storing the original and current orientation vectors, but i am cooked rn
+    [field: NonSerialized] public List<Vector2Int> orientedShape;
+    [field: NonSerialized] public SpriteRenderer spriteRenderer{get;private set;}
 
     // Stuff for score logic
     public int score = 0;
@@ -31,9 +35,10 @@ public class Prop : MonoBehaviour
 
         // in case the rotation by default is not 0
         int num_rotations = (int) transform.rotation.eulerAngles.z % 360;
-        num_rotations = (num_rotations > 0) ? 360 - num_rotations : num_rotations;
+        num_rotations = (num_rotations > 0) ? 360 - num_rotations : -num_rotations;
         num_rotations /= 90;
         transform.rotation = Quaternion.identity;
+        orientedShape = new List<Vector2Int>(shape);
         for(int i = 0; i < num_rotations;i++){
             rotate_90();
         }
@@ -47,13 +52,13 @@ public class Prop : MonoBehaviour
         transform.Rotate(0,0,-90); // rotate the image
 
         // rotate the shape list. This makes sure that checking cells works no matter the orientation 
-        for(int i = 0; i < shape.Length; i++){
-            Vector2Int cell = shape[i];
+        for(int i = 0; i < orientedShape.Count; i++){
+            Vector2Int cell = orientedShape[i];
             // to understand more of whats happening here, look up rotation matrices
             int new_x = cell.y;
             int new_y = -cell.x;
             Vector2Int rotated_cell = new Vector2Int(new_x,new_y);
-            shape[i] = rotated_cell;
+            orientedShape[i] = rotated_cell;
         }
     }
 
@@ -61,15 +66,14 @@ public class Prop : MonoBehaviour
         get{return gridLocation;}
         set{
             gridLocation = value;
-            Vector3 cell_center = Map.GetCellCenterWorld(gridLocation); 
-            transform.position = cell_center;
+            // Vector3 cell_center = Map.GetCellCenterWorld(gridLocation); 
+            transform.position = Map.GetCellCenterWorld(gridLocation);
         }
     }
 
     // this may be more computationally efficient. Instead of updating every frame, only check on mouse click. But eh, whatever.
     // private void OnMouseUp()
     // {
-    //     return; // For now, just don't do anything
     //     // left click - get info from selected tile
     //     if (Input.GetMouseButtonUp(0))
     //     {
